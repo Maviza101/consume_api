8058fc594fa121273799df78a0cbb5e7561b6308
Folusho Oladipo <Maviza101@users.noreply.github.com>
2016-08-09 12:25:46 +0100

    edited vars and comments to improve code clarity

--- consume_api.js
+++ consume_api.js
@@ -8,13 +8,13 @@
           });
           /*Note: you have to get your own client_id and client_secret from 
           GitHub OAuth applications. The functional ones that were here before
-          have been deleted on GitHub and removed here, and the ones used here 
+          have been deleted on GitHub and removed from this script. The ones used here 
           are obviously placeholders.*/
           var clientId = 123
           var clientSecret = 123
-          function getUserData (user_url) {
+          function getUserData (user) {
             $.ajax({
-        url: "https://api.github.com/users/"+user_url+"?client_id=" + clientId + "&client_secret=" + clientSecret
+        url: "https://api.github.com/users/"+user+"?client_id=" + clientId + "&client_secret=" + clientSecret
     }).then(function(data) {
             $('#user_id .returned_data').append(data.id);
             $('#user_login .returned_data').append(data.login);
